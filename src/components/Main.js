c3e9921871373c0aebf8a88e9ebe8344798feb3c
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-08-30 16:44:43 -0300

    firist commit

---
+++ src/components/Main.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+// Form
+import { FaPlus } from 'react-icons/fa'
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
+import './Main.css';
+
+export default class Main extends Component {
+	state = {
+		novaTarefa: '',
+		tarefas: [
+			'Fazer café',
+			'Beber água',
+			'Estudar',
+		],
+	};
+
+	handleChange = (e) => {
+		this.setState({
+				novaTarefa: e.target.value
+		}); 
+	}
+
+	render() {
+		const { novaTarefa, tarefas } = this.state;
+
+		return (
+			<div className="main">
+				<h1>Lista de tarefas</h1> 
+
+				<form action="#" className="form">
+					<input 
+						onChange={this.handleChange} 
+						type="text" 
+						value={novaTarefa}
+					/>
+					<button type="submit">
+						<FaPlus />  
+					</button>
+				</form>
+
+				<ul className="tarefas">
+					{tarefas.map(tarefa => (
+						<li key={tarefa}>
+							{tarefa}
+							<div>
+							<FaEdit className="edit"/>
+							< FaWindowClose className="delete"/>	
+							</div> 
+							</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
+}
