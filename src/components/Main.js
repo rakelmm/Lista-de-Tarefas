3b1b2c618f5f8ceb8ae11229950e54b5559f190e
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-08-31 14:56:32 -0300

    Add tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,16 +9,26 @@ import './Main.css';
 export default class Main extends Component {
 	state = {
 		novaTarefa: '',
-		tarefas: [
-			'Fazer café',
-			'Beber água',
-			'Estudar',
-		],
+		tarefas: [],
 	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		const { tarefas } = this.state;
+		let { novaTarefa } = this.state;
+		novaTarefa = novaTarefa.trim();
+
+		if(tarefas.indexOf(novaTarefa) !== -1) return;
+
+		const novasTarefas = [...tarefas];
+		this.setState({
+			tarefas: [...novasTarefas, novaTarefa],
+		});
+	}
 
 	handleChange = (e) => {
 		this.setState({
-				novaTarefa: e.target.value
+				novaTarefa: e.target.value,
 		}); 
 	}
 
@@ -29,7 +39,7 @@ export default class Main extends Component {
 			<div className="main">
 				<h1>Lista de tarefas</h1> 
 
-				<form action="#" className="form">
+				<form onSubmit={this.handleSubmit} action="#" className="form">
 					<input 
 						onChange={this.handleChange} 
 						type="text" 
@@ -44,10 +54,10 @@ export default class Main extends Component {
 					{tarefas.map(tarefa => (
 						<li key={tarefa}>
 							{tarefa}
-							<div>
+							<span>
 							<FaEdit className="edit"/>
 							< FaWindowClose className="delete"/>	
-							</div> 
+							</span> 
 							</li>
 					))}
 				</ul>
